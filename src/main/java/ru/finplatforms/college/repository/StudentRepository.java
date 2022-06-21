35b7b29fb921cfffb461cf04d3848c99d62a10f8
alexcypriiz <alexey.podlubny@yandex.ru>
2022-06-21 23:39:29 +0400

    feat: Add service for student

--- src/main/java/ru/finplatforms/college/repository/StudentRepository.java
+++ src/main/java/ru/finplatforms/college/repository/StudentRepository.java
@@ -1,8 +1,10 @@
 package ru.finplatforms.college.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.finplatforms.college.model.Student;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student,Long> {
 
 }
---
+++ src/main/java/ru/finplatforms/college/service/StudentService.java
@@ -0,0 +1,35 @@
+package ru.finplatforms.college.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.finplatforms.college.model.Student;
+import ru.finplatforms.college.repository.StudentRepository;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    final
+    StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> findAll() {
+        return studentRepository.findAll();
+    }
+
+    public Student findById(Long id) {
+        return studentRepository.getOne(id);
+    }
+
+    public Student saveClient(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public void delete(Long id) {
+        studentRepository.deleteById(id);
+    }
+}
